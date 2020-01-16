fb308c084bde97cec7af4ddc411778f0ac48826b
NeculaiHaisuc <NECULAI.HAISUC@feaa.uaic.ro>
2020-01-16 23:56:00 +0200

    Change all file structure.

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!!'
-
+@app.route("/")
+def hello():
+    return "Hello World!"
